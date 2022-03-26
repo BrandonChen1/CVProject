16679785cf333e50ce1f7ad190d5c6587296c046
Jack <jakexu9946@gmail.com>
2022-03-26 16:31:18 -0400

    blah

--- SetSolver.m
+++ SetSolver.m
@@ -4,9 +4,13 @@ function SetSolver(fileName)
 end
 
 function defineBlackBoxes(fileName)
-    image_in = "~/Images/IMG_7534.jpg";
+    image_in = "Images/IMG_7666.jpg";
     im_orig = imread(image_in);
+    padvalue = 0; % or 1 if image is single, double, or logical.
+    im_orig = padarray(im_orig, [20,20],255);
     grayscaleImage = rgb2gray(im_orig);
+    %figure
+    %imshow(im_orig);
     %figure
     %imshow(grayscaleImage);
     bwImage = im2bw(grayscaleImage,.3);
@@ -16,13 +20,16 @@ function defineBlackBoxes(fileName)
     bwImageOpened = imopen(bwImage, se);
     bwImageClosed = imclose(bwImageOpened,se);
     figure
-    imshow(bwImageClosed)
+    imshow(bwImageClosed);
     stats = regionprops(bwImageClosed,'all');
     for idx = 1:size(stats)
         boundingBox = stats(idx).BoundingBox;
-        cropped_image = imcrop(im_orig, boundingBox);
-        figure;
-        imshow(cropped_image);
+%         cropped_image = imcrop(im_orig, boundingBox);
+        hold on 
+        rectangle('Position', [boundingBox(1), boundingBox(2), boundingBox(3), boundingBox(4)] , 'Edgecolor' , 'c', 'LineWidth', 2);
+
+        %figure;
+        %imshow(cropped_image);
     end
 end
 
