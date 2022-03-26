b1c6e505d9687dd6e7feab4281521a545cfdca82
Jack <jakexu9946@gmail.com>
2022-03-26 16:23:11 -0400

    bounding boxes

--- SetSolver.m
+++ SetSolver.m
@@ -1,23 +1,28 @@
 function SetSolver(fileName)
-    fileName = "IMG_7542.jpg"
+    fileName = "~/Images/IMG_7534.jpg"
     defineBlackBoxes(fileName)
 end
 
 function defineBlackBoxes(fileName)
-    image_in = "IMG_7542.jpg";
+    image_in = "~/Images/IMG_7534.jpg";
     im_orig = imread(image_in);
     grayscaleImage = rgb2gray(im_orig);
-    figure
-    imshow(grayscaleImage);
+    %figure
+    %imshow(grayscaleImage);
     bwImage = im2bw(grayscaleImage,.3);
-    figure
-    imshow(bwImage)
+    %figure
+    %imshow(bwImage)
     se = strel("disk", 8);
     bwImageOpened = imopen(bwImage, se);
     bwImageClosed = imclose(bwImageOpened,se);
     figure
     imshow(bwImageClosed)
     stats = regionprops(bwImageClosed,'all');
-    allBoundingBox = stats(:).BoundingBox;
+    for idx = 1:size(stats)
+        boundingBox = stats(idx).BoundingBox;
+        cropped_image = imcrop(im_orig, boundingBox);
+        figure;
+        imshow(cropped_image);
+    end
 end
 
